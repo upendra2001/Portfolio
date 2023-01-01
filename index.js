255ab1fa4616cb3b48ad2895cf8470e65a0e7e27
upendra2001 <61980545+upendra2001@users.noreply.github.com>
2023-01-01 19:01:43 +0530

    added animations

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ $(document).ready(function () {
     let themeToggler = document.querySelector('#theme-toggler');
 
     themeToggler.onclick = () => {
-        if (themeToggler.classList.contains('fa-moon')){
+        if (themeToggler.classList.contains('fa-moon')) {
             themeToggler.classList.remove('fa-moon');
             themeToggler.classList.add('fa-sun');
         }
-        else{
+        else {
             themeToggler.classList.remove('fa-sun');
             themeToggler.classList.add('fa-moon');
 
@@ -54,11 +54,60 @@ $(document).ready(function () {
     }
 
 });
+$(window).scroll(function () {
+    if (screen.width <= 503) {
+        if ($(this).scrollTop() > 1000) {
+            $('#education-box1').addClass('animate__animated animate__slideInLeft animate__slower');
+            $('#education-box2').addClass('animate__animated animate__slideInRight animate__slower');
+            $('#education-box3').addClass('animate__animated animate__slideInLeft animate__slower');
+        } else {
+            $('#education-box1').removeClass('animate__animated animate__slideInLeft animate__slower');
+            $('#education-box2').removeClass('animate__animated animate__slideInRight animate__slower');
+            $('#education-box3').removeClass('animate__animated animate__slideInLeft animate__slower');
+        }
+        if ($(this).scrollTop() > 500) {
+            $('#about-box1').addClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box3').addClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box2').addClass('animate__animated animate__slideInRight animate__slower');
+            $('#about-box4').addClass('animate__animated animate__slideInRight animate__slower');
+        }
+        else {
+            $('#about-box1').removeClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box3').removeClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box2').removeClass('animate__animated animate__slideInRight animate__slower');
+            $('#about-box4').removeClass('animate__animated animate__slideInRight animate__slower');
+        }
+    }
+    else {
+        if ($(this).scrollTop() > 1000) {
+            $('#education-box1').addClass('animate__animated animate__slideInDown animate__slower');
+            $('#education-box2').addClass('animate__animated animate__zoomIn animate__slower');
+            $('#education-box3').addClass('animate__animated animate__slideInDown animate__slower');
+
+        } else {
+            $('#education-box1').removeClass('animate__animated animate__slideInDown animate__slower');
+            $('#education-box2').removeClass('animate__animated animate__zoomIn animate__slower');
+            $('#education-box3').removeClass('animate__animated animate__slideInDown animate__slower');
+        }
+        if ($(this).scrollTop() > 500) {
+            $('#about-box1').addClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box3').addClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box2').addClass('animate__animated animate__slideInRight animate__slower');
+            $('#about-box4').addClass('animate__animated animate__slideInRight animate__slower');
+        }
+        else {
+            $('#about-box1').removeClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box3').removeClass('animate__animated animate__slideInLeft animate__slower');
+            $('#about-box2').removeClass('animate__animated animate__slideInRight animate__slower');
+            $('#about-box4').removeClass('animate__animated animate__slideInRight animate__slower');
+        }
+    }
+});
 
 //code for auto typing animation
-var shortintro="Hi 👋, I am Upendra Singh Chauhan";
+var shortintro = "Hi 👋, I am Upendra Singh Chauhan";
 
-var ele1='<h1>' + shortintro.split('').join('</h1><h1>') + '</h1>';
+var ele1 = '<h1>' + shortintro.split('').join('</h1><h1>') + '</h1>';
 
 $(ele1).hide().appendTo('#short-intro').each(function (i) {
     $(this).delay(100 * i).css({
